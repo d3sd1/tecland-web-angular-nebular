5bc995026d8ba57ed38a6a34127cd54559803f9b
Andrei García (Sobremesa) <andreigarciacuadra@gmail.com>
2019-04-16 22:09:26 +0200

    actualizado copyright

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+
+    <span class="created-by">Copyright <b>TecLand</b>. Todos los derechos reservados.</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+
+      <!--<a href="#" class="ion ion-arrow-up-c"></a>-->
     </div>
   `,
 })
